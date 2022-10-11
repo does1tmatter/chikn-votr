2eb87bad98d8600c5edbe4d94addec8b81a4398c
breadNbutter42 <97899677+breadNbutter42@users.noreply.github.com>
2022-10-11 03:22:51 -0400

    Update LoadingOverlay.vue

--- src/components/LoadingOverlay.vue
+++ src/components/LoadingOverlay.vue
@@ -1,6 +1,6 @@
 <template>
   <Transition name="fade">
-    <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full backdrop-blur-[2px]">
+    <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full backdrop-blur-[0px]">
       <Spinner class="w-8 h-8" />
     </div>
   </Transition>
