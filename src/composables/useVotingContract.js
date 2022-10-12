28d174fb3afe5f8ca9832c0222ac8b59cfa52228
breadNbutter42 <97899677+breadNbutter42@users.noreply.github.com>
2022-10-12 05:29:16 -0400

    Update useVotingContract.js

--- src/composables/useVotingContract.js
+++ src/composables/useVotingContract.js
@@ -16,7 +16,7 @@ export default (address) => {
   
   const prizeMoneyTotalWei = async () => await contract.prizeMoneyTotalWei().then(response => Number(ethers.utils.formatEther(response)))
   const eggBurntTotalWei = async () => await contract.eggBurntTotalWei().then(response => Number(ethers.utils.formatEther(response)))
-  const allVotesTotalBase = async () => await contract.allVotesTotalBase().then(response => Number(response))
+  const allVotesTotalBase = async () => await contract.allVotesTotalBase().then(response => String(Number(response)))
   const votingTimeLeftBlockTimestampHours = async () => await contract.votingTimeLeftBlockTimestamp().then(response => Math.floor(Number(response) / 3600))
   const totalVotesFromVoterAddress = async (payload) => await contract.totalVotesFromVoterAddress(payload ?? address.value).then(response => Number(response))
   const addressTotalVotesForIDNumber = async (payload, id) => await contract.addressTotalVotesForIDNumber(payload ?? address.value, id).then(response => Number(response))
