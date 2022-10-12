281dd4e68b78241dad164572ca153733d800e48b
breadNbutter42 <97899677+breadNbutter42@users.noreply.github.com>
2022-10-12 01:16:27 -0400

    Update useVotingContract.js

--- src/composables/useVotingContract.js
+++ src/composables/useVotingContract.js
@@ -10,15 +10,16 @@ export default (address) => {
   let contract
   const setContract = (payload) => contract = new ethers.Contract(import.meta.env.VITE_CONTRACT_VOTING, abi, payload)
 
-  const returnTotalVotesForCandidateIDNumber = async (id) => await contract.returnTotalVotesForCandidateIDNumber(id).then(response => Number(response))
   const voteWithEggByCandidateNumber = async (id, amt) => await contract.voteWithEggByCandidateNumber(id, amt)
   const voteOneEggForEachCandidate = async () => await contract.voteOneEggForEachCandidate()
+  const returnTotalVotesForCandidateIDNumber = async (id) => await contract.returnTotalVotesForCandidateIDNumber(id).then(response => Number(response))
   
-  const prizeMoneyTotalWei = async () => await contract.prizeMoneyTotalWei()
-  const eggBurntTotalWei = async () => await contract.eggBurntTotalWei()
-  const allVotesTotalBase = async () => await contract.allVotesTotalBase().then(res => Number(res))
-  const totalVotesFromVoterAddress = async () => await contract.totalVotesFromVoterAddress().then(res => Number(res))
-  const addressTotalVotesForIDNumber = async () => await contract.addressTotalVotesForIDNumber().then(res => Number(res))
+  const prizeMoneyTotalWei = async () => await contract.prizeMoneyTotalWei().then(response => Number(ethers.utils.formatEther(response)))
+  const eggBurntTotalWei = async () => await contract.eggBurntTotalWei().then(response => Number(ethers.utils.formatEther(response)))
+  const allVotesTotalBase = async () => await contract.allVotesTotalBase().then(response => Number(response))
+  const votingTimeLeftBlockTimestampHours = async () => await contract.votingTimeLeftBlockTimestamp().then(response => Math.floor(Number(response) / 3600))
+  const totalVotesFromVoterAddress = async (payload) => await contract.totalVotesFromVoterAddress(payload ?? address.value).then(response => Number(response))
+  const addressTotalVotesForIDNumber = async (payload, id) => await contract.addressTotalVotesForIDNumber(payload ?? address.value, id).then(response => Number(response))
 
   
   userStore.$onAction(({ name, after }) => {
@@ -38,6 +39,11 @@ export default (address) => {
     contract,
     returnTotalVotesForCandidateIDNumber,
     voteWithEggByCandidateNumber,
-    voteOneEggForEachCandidate
+    voteOneEggForEachCandidate,
+    prizeMoneyTotalWei,
+    eggBurntTotalWei,
+    allVotesTotalBase,
+    totalVotesFromVoterAddress,
+    votingTimeLeftBlockTimestampHours
   }
 }
