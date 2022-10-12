da59d99824090ef67d1ba82822ef287f32f62f6b
breadNbutter42 <97899677+breadNbutter42@users.noreply.github.com>
2022-10-12 03:09:39 -0400

    Update Candidate.vue

--- src/components/Candidate.vue
+++ src/components/Candidate.vue
@@ -84,9 +84,6 @@ onAppEvent(({ type, payload }) => {
         <div class="mt-1 text-sm">
           Total Votes: <span class="text-gold-500">{{ votes }}</span>
         </div>
-        <div class="mt-1 text-sm">
-          Your Votes: <span class="text-gold-500">{{ votes }}</span>
-        </div>
         <div class="text-xs mt-2">
           Candidate ID: <span class="text-gold-500">{{ candidate.id }}</span>
         </div>
